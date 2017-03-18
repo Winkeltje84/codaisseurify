bfd0ddf64094604b61d96dbd8c116cee18e4e488
Winkeltje84 <bwinkel@gmail.com>
2017-03-18 17:53:12 +0100

    added models/controller/seeds

--- Gemfile
+++ Gemfile
@@ -38,6 +38,12 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  # => Rspec for testing
+  gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
+  # => FactoryGirl for usage in testing
+  gem 'factory_girl_rails', '4.7.0'
+  # => Faker for creating fake data in FactoryGirl or Seeds
+  gem 'faker', '1.6.6'
 end
 
 group :development do
@@ -52,11 +58,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-group :development, :test do
-  # => Rspec for testing
-  gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
-end
-
 group :test do
   # => Capybara for testing
   gem 'capybara', '~> 2.9', '>= 2.9.1'
@@ -64,8 +65,3 @@ end
 
 # => Bootstrap for styling
 gem 'bootstrap-sass', '~> 3.3.6'
-
-# => FactoryGirl for usage in testing
-gem 'factory_girl_rails', '4.7.0'
-# => Faker for creating fake data in FactoryGirl or Seeds
-gem 'faker', '1.6.6'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.delete_all
+Artist.delete_all
+
+artist1 = Artist.create!(name: "Kraantje Pappie", image: "http://res.cloudinary.com/dqaserzum/image/upload/v1489853383/Kraantje-Pappie-Pers-2-kopie_csprr9.jpg")
+artist2 = Artist.create!(name: "The Bloody Beetroots", image: "http://res.cloudinary.com/dqaserzum/image/upload/v1489853576/the-bloody-beetrootsarticle_ee1xyd.jpg")
+artist3 = Artist.create!(name: "Parov Stelar", image: "http://res.cloudinary.com/dqaserzum/image/upload/v1489853715/Parov-Stelar_Sappert004-web_pbqgqz.jpg")
+artist4 = Artist.create!(name: "Tame Impala", image: "http://res.cloudinary.com/dqaserzum/image/upload/v1489854121/Tame-Impala_s04ini.jpg")
+
+
+
+Song.create!(name: "De Manier", artist: artist1)
+Song.create!(name: "Feesttent", artist: artist1)
+Song.create!(name: "Pompen", artist: artist1)
+
+Song.create!(name: "Rocksteady", artist: artist2)
+Song.create!(name: "Chronicles of a Fallen Love", artist: artist2)
+
+Song.create!(name: "All Night", artist: artist3)
+Song.create!(name: "Booty Swing", artist: artist3)
+Song.create!(name: "Catgroove", artist: artist3)
+
+Song.create!(name: "The Less I Know the Better", artist: artist4)
+Song.create!(name: "'Cause I'm a Man'", artist: artist4)
+
+
+=begin
+create_table "artists", force: :cascade do |t|
+  t.string   "name"
+  t.string   "image"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+
+create_table "songs", force: :cascade do |t|
+  t.string   "name"
+  t.integer  "artist_id"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+  t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+=end
